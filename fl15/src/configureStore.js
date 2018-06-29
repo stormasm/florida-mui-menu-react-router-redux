1c84a8700f3090d91281c3183d1b0d1197ca9a93
Michael Angerman <stormasm@gmail.com>
2018-06-29 13:44:03 -0700

    working on configureStore

--- fl15/src/configureStore.js
+++ fl15/src/configureStore.js
@@ -3,12 +3,16 @@ import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
 import rootReducer from "./reducers";
 
+import createHistory from 'history/createBrowserHistory'
+import { routerMiddleware } from 'react-router-redux'
+
 const loggerMiddleware = createLogger();
+const history = createHistory();
 
 export default function configureStore(preloadedState) {
   return createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunkMiddleware, loggerMiddleware)
+    applyMiddleware(routerMiddleware(history), thunkMiddleware, loggerMiddleware)
   );
 }
