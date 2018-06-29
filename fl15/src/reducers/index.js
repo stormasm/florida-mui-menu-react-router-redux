b7f00198343464c668b0f7a9220779c4cb97fcae
Michael Angerman <stormasm@gmail.com>
2018-06-29 13:51:26 -0700

    add in location to the reducer function

--- fl15/src/reducers/index.js
+++ fl15/src/reducers/index.js
@@ -1,9 +1,12 @@
 import { combineReducers } from "redux";
 import { keys, selectedKey } from "./key";
 
+import { routerReducer } from 'react-router-redux'
+
 const rootReducer = combineReducers({
   keys,
-  selectedKey
+  selectedKey,
+  routerReducer
 });
 
 export default rootReducer;
